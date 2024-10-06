e91260cc325483c59d2d9dbdbd3a6d3a332d95c6
ab1608 <abebriones98@gmail.com>
2024-10-05 20:05:16 -0400

    pset

--- class19.R
+++ class19.R
@@ -1,16 +1,18 @@
-# Chi-square test for goodness of fit
-outcomes <- 0:6
-observed <- c(5, 11, 13, 7, 2, 1, 1)
-null_probs <- c(dbinom(outcomes, 6, 0.4))
-n <- sum(observed)
-expected_counts <- n * null_probs
+# Class 19 Problem Set
+# Chi-square test for goodness of fit for Problem 1
+owner_dist <- c(0.1, 0.1, 0.15, 0.2, 0.3, 0.15)
+observed <- c(34, 14, 34, 45, 57, 20)
+total_customers <- sum(observed)
+expected_customes <- total_customers * owner_dist
 df <- length(observed) - 1
 
 # Pearon's chi-squared statistic
 x2 <- sum((observed - expected_counts)^2 / expected_counts)
 
 # Likelihood ratio statistic
-g <- 2 * sum(observed * log(observed / expected_counts))
+g <- 2 * sum(observed * log(observed / expected_customes))
 
 p_x2 <- 1 - pchisq(x2, df)
 p_g <- 1 - pchisq(g, df)
+print(p_x2)
+print(p_g)
